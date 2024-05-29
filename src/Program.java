64515d52e0d6f23bdd1a4cfcf24f583ce85f3075
Lauro Augusto <lauroafp@gmail.com>
2024-05-29 14:36:49 -0300

    Commit 2 commit dois

--- src/Program.java
+++ src/Program.java
@@ -1,7 +1,7 @@
 public class Program {
     public static void main(String[] args) {
 
-        Position pos = new Position(3, 5);
-        System.out.println(pos);
+        Board board = new Board(8, 8);
+
     }
 }
