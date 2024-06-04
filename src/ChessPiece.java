cdfba9e859aa7f5724ddf2f2873a41e703172924
Lauro Augusto <lauroafp@gmail.com>
2024-06-04 15:50:03 -0300

    Commit 18 -Implementando a Lógica de contagem de Peças

--- src/ChessPiece.java
+++ src/ChessPiece.java
@@ -1,6 +1,7 @@
 public abstract class ChessPiece extends Piece {
 
     private Color color;
+    private int moveCount;
 
     public ChessPiece(Board board, Color color) {
         super(board);
@@ -9,6 +10,18 @@ public abstract class ChessPiece extends Piece {
 
     public Color getColor() {
         return color;
+    }
+
+    public int getMoveCount() {
+        return moveCount;
+    }
+
+    public void increaseMoveCount() {
+        moveCount++;
+    }
+
+    public void decreaseMoveCount() {
+        moveCount--;
     }
 
     public ChessPosition getChessPosition() {
